765a417b6315dd55cf1f911901d5fc5b3c041709
Alan M. Turing <1912AlanTuring1954@gmail.com>
2022-09-11 19:03:15 -0700

    Initial Commit.

---
+++ sketch.js
@@ -0,0 +1,36 @@
+let particles = [];
+const num = 4500;
+const noiseScale = 0.01;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < num; i++) {
+    particles.push(createVector(random(windowWidth), random(windowHeight)));
+  }
+  stroke(255);
+}
+
+function draw() {
+  background(0, 10);
+  frameRate(50);
+  for (let i = 0; i < num; i++) {
+    let p = particles[i];
+    point(p.x, p.y);
+    let n = noise(p.x * noiseScale, p.y * noiseScale);
+    let a = TAU * n;
+    p.x += cos(a);
+    p.y += sin(a);
+    if (!onScreen(p)) {
+      p.x = random(windowWidth);
+      p.y = random(windowHeight);
+    }
+  }
+}
+
+function mouseReleased() {
+  noiseSeed(millis());
+}
+
+function onScreen(v) {
+  return v.x >= 0 && v.x <= windowWidth && v.y >= 0 && v.y <= windowHeight;
+}
